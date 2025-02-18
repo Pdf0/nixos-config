22a2c459a123d1b5797ff2d347f75c9e268070b4
Pdf0 <pedro.er.sousa@gmail.com>
2025-02-18 09:25:19 +0000

    Modularize pkgs

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./cachix.nix
       ./users/pdf.nix
+      ./programs/pkgs.nix
       ./programs/zsh.nix
       ./services/nvidia.nix
       ./services/git.nix
@@ -80,80 +81,6 @@
   systemd.services.polkit-gnome-authentication-agent-1 = {
     serviceConfig.User = "pdf";
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    openvpn
-    p7zip
-    bat
-    brave
-    spotify
-    vesktop
-    alsa-utils
-    findutils
-    terminator
-    vscode
-    polkit
-    polkit_gnome
-    python311
-    python311Packages.pip
-    python311Packages.dbus-python
-    flameshot
-    lshw
-    git
-    tldr
-    vlc
-    pulseaudio
-    cachix
-    libreoffice
-    brightnessctl
-    obsidian
-    qemu
-    maven
-    sumo
-    nix-index
-    openjdk
-    polybar
-    font-awesome
-    rofi
-    bluez
-    gpick
-    alacritty
-    xfce.thunar
-    okular
-    gnome-calculator
-    zoxide
-    feh
-
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        bbenoist.nix
-        ms-python.python
-      ];
-    })
-
-    (symlinkJoin {
-      name = "discord";
-      paths = [ vesktop ];
-      postBuild = ''
-        ln -s $out/bin/vesktop $out/bin/discord
-      '';
-    })
-
-    (symlinkJoin {
-      name = "calculator";
-      paths = [ gnome-calculator ];
-      postBuild = ''
-        ln -s $out/bin/gnome-calculator $out/bin/calculator
-      '';
-    })
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
---
+++ programs/pkgs.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ...}:
+
+{
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        vim
+        wget
+        openvpn
+        p7zip
+        bat
+        brave
+        spotify
+        vesktop
+        alsa-utils
+        findutils
+        terminator
+        vscode
+        polkit
+        polkit_gnome
+        python311
+        python311Packages.pip
+        python311Packages.dbus-python
+        flameshot
+        lshw
+        git
+        tldr
+        vlc
+        pulseaudio
+        cachix
+        libreoffice
+        brightnessctl
+        obsidian
+        qemu
+        maven
+        sumo
+        nix-index
+        openjdk
+        polybar
+        font-awesome
+        rofi
+        bluez
+        gpick
+        alacritty
+        xfce.thunar
+        okular
+        gnome-calculator
+        zoxide
+        feh
+
+        (vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+            bbenoist.nix
+            ms-python.python
+        ];
+        })
+
+        (symlinkJoin {
+        name = "discord";
+        paths = [ vesktop ];
+        postBuild = ''
+            ln -s $out/bin/vesktop $out/bin/discord
+        '';
+        })
+
+        (symlinkJoin {
+        name = "calculator";
+        paths = [ gnome-calculator ];
+        postBuild = ''
+            ln -s $out/bin/gnome-calculator $out/bin/calculator
+        '';
+        })
+    ];
+}
